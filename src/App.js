d3a0d46a357681c6d863ce9e0083f15347de64cf
gacolombo <gcolomboaraujo@gmail.com>
2023-04-13 14:15:22 -0300

    uploading an existing project that will be used to study Reactjs

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1 title="This works!">Hi, this is React!</h1>; // => React.createElement('h1', {title: 'This works!', ...})
+};
+
+export default App;
