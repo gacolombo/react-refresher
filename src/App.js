703cf54a730146e63421830a7cfbb2d6fadbbc97
gacolombo <gcolomboaraujo@gmail.com>
2023-04-13 14:39:28 -0300

    adicionando arquivo css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import './App.css';
+
 const App = () => {
-  return <h1 title="This works!">Hi, this is React!</h1>; // => React.createElement('h1', {title: 'This works!', ...})
+  return (
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <ul className='goal-list'>
+        <li>Finish the Course</li>
+        <li>Learn all about the course main topic</li>
+        <li>Help other students in the Course Q&amp;E </li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
